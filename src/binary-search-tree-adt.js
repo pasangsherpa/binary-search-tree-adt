7fdf54af724e278e69d8c37e8dc4f27437d70387
Pasang Sherpa <pasang_sh@hotmail.com>
2014-08-24 22:38:07 -0700

    test added

--- src/binary-search-tree-adt.js
+++ src/binary-search-tree-adt.js
@@ -1,50 +1,74 @@
 (function() {
 	'use strict';
 	var BinarySearchTree = function() {
+		var root = null,
+			count = 0;
+
+		function Node(element) {
+			this.element = element || null;
+			this.right = this.left = null;
+		}
 
 		function add(element) {
 
 		}
 
 		function remove(element) {
-
+			if (isEmpty()) {
+				throw new Error("removeMin(): Tree is empty.");
+			}
 		}
 
 		function removeAllOccurrence(element) {
-
+			if (isEmpty()) {
+				throw new Error("removeMin(): Tree is empty.");
+			}
 		}
 
 		function removeMin() {
-
+			if (isEmpty()) {
+				throw new Error("removeMin(): Tree is empty.");
+			}
 		}
 
 		function removeMax() {
-
+			if (isEmpty()) {
+				throw new Error("removeMin(): Tree is empty.");
+			}
 		}
 
 		function getRoot() {
-
+			if (isEmpty()) {
+				throw new Error("removeMin(): Tree is empty.");
+			}
 		}
 
 		function find(element) {
-
+			if (isEmpty()) {
+				throw new Error("removeMin(): Tree is empty.");
+			}
 		}
 
 		function findMin() {
-
+			if (isEmpty()) {
+				throw new Error("removeMin(): Tree is empty.");
+			}
 		}
 
 		function findMax() {
-
+			if (isEmpty()) {
+				throw new Error("removeMin(): Tree is empty.");
+			}
 		}
 
 		function isEmpty() {
-
+			return count === 0;
 		}
 
 		function size() {
-
+			return count;
 		}
+
 		return {
 			add: add,
 			remove: remove,
--- test/test.js
+++ test/test.js
@@ -1,4 +1,19 @@
 /*global describe, it */
 'use strict';
 var assert = require('assert');
-var qs = require('../src/binary-tree-adt.js');
+var BinarySearchTree = require('../src/binary-search-tree-adt.js');
+
+var bst = new BinarySearchTree();
+
+describe('BinarySearchTree operations test', function() {
+
+	it('should create an empty tree in the beginning', function() {
+		assert.equal(bst.isEmpty(), true);
+		assert.equal(bst.size(), 0);
+		assert.throws(function() {
+			bst.removeMin();
+			throw new Error('removeMin(): Tree is empty');
+		}, Error);
+	});
+
+});
