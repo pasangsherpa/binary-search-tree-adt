513fa2f8254b644cfbed0d2a435357b0df560c7a
Pasang Sherpa <pasang_sh@hotmail.com>
2014-08-26 23:42:11 -0700

    Add operation fix + browser test.html added

--- src/binary-search-tree-adt.js
+++ src/binary-search-tree-adt.js
@@ -10,37 +10,71 @@
 		}
 
 		function add(element) {
-
+			var node = new Node(element);
+			root = addTo(root, node);
+			count++;
+			return node.element;
 		}
 
+		function addTo(current, node) {
+			if (current === null) {
+				return node;
+			}
+			if (node.element < current.element) {
+				current.left = addTo(current.left, node);
+			} else {
+				current.right = addTo(current.right, node);
+			}
+			return current;
+		}
+
+		// TODO: Get accessor before removing
 		function remove(element) {
 			if (isEmpty()) {
 				throw new Error("removeMin(): Tree is empty.");
 			}
 		}
 
+		// TODO: Get accessor before removing
 		function removeAllOccurrence(element) {
 			if (isEmpty()) {
 				throw new Error("removeMin(): Tree is empty.");
 			}
 		}
 
+		// TODO: Get accessor before removing
 		function removeMin() {
 			if (isEmpty()) {
 				throw new Error("removeMin(): Tree is empty.");
 			}
+			var current = root,
+				previous = root;
+			while (current.left !== null) {
+				previous = current;
+				current = current.left;
+			}
+			previous.left = null;
 		}
 
+		// TODO: Get accessor before removing
 		function removeMax() {
 			if (isEmpty()) {
 				throw new Error("removeMin(): Tree is empty.");
 			}
+			var current = root,
+				previous = root;
+			while (current.right !== null) {
+				previous = current;
+				current = current.right;
+			}
+			previous.right = null;
 		}
 
 		function getRoot() {
 			if (isEmpty()) {
 				throw new Error("removeMin(): Tree is empty.");
 			}
+			return root;
 		}
 
 		function find(element) {
