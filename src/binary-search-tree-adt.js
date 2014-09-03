5151dfaef71a5924c3166c7da874fc2cd288b255
Pasang Sherpa <pasang_sh@hotmail.com>
2014-09-02 23:58:52 -0700

    add, findMin, findMax operations fix

--- src/binary-search-tree-adt.js
+++ src/binary-search-tree-adt.js
@@ -6,43 +6,55 @@
 
 		function Node(element) {
 			this.element = element || null;
-			this.right = this.left = null;
+			this.parent = this.right = this.left = null;
 		}
 
 		function add(element) {
-			var node = new Node(element);
-			root = addAgain(root, node);
+			var node = new Node(element),
+				current, parent;
+			current = parent = root;
+
+			while (current !== null) {
+				if (element > current.element) {
+					parent = current;
+					current = current.right;
+				} else {
+					parent = current;
+					current = current.left;
+				}
+			}
+
+			if (parent === null) {
+				root = node;
+				return;
+			}
+
+			if (parent.element > element) {
+				parent.left = node;
+			} else {
+				parent.right = node;
+			}
+
+			node.parent = parent;
 			count++;
 			return node.element;
 		}
 
-		function addAgain(next, node) {
-			if (next === null) {
-				return node;
-			}
-			if (node.element < next.element) {
-				next.left = addAgain(next.left, node);
-			} else {
-				next.right = addAgain(next.right, node);
-			}
-			return next;
-		}
-
-		// TODO: Get accessor before removing
+		// TODO: Get successor before removing
 		function remove(element) {
 			if (isEmpty()) {
-				throw new Error('removeMin(): Tree is empty.');
+				throw new Error('remove(): Tree is empty.');
 			}
 		}
 
-		// TODO: Get accessor before removing
+		// TODO: Get successor before removing
 		function removeAllOccurrence(element) {
 			if (isEmpty()) {
-				throw new Error('removeMin(): Tree is empty.');
+				throw new Error('removeAllOccurrence(): Tree is empty.');
 			}
 		}
 
-		// TODO: Get accessor before removing
+		// TODO: Get successor before removing
 		function removeMin() {
 			if (isEmpty()) {
 				throw new Error('removeMin(): Tree is empty.');
@@ -56,10 +68,10 @@
 			previous.left = null;
 		}
 
-		// TODO: Get accessor before removing
+		// TODO: Get successor before removing
 		function removeMax() {
 			if (isEmpty()) {
-				throw new Error('removeMin(): Tree is empty.');
+				throw new Error('removeMax(): Tree is empty.');
 			}
 			var current = root,
 				previous = root;
@@ -72,7 +84,7 @@
 
 		function getRoot() {
 			if (isEmpty()) {
-				throw new Error('removeMin(): Tree is empty.');
+				throw new Error('getRoot(): Tree is empty.');
 			}
 			return root;
 		}
@@ -102,26 +114,34 @@
 			return temp;
 		}
 
-		function findMin() {
-			if (isEmpty()) {
-				throw new Error('removeMin(): Tree is empty.');
-			}
-			var current = root;
-			while(current.left !== null) {
+		function findMinNode(node) {
+			var current = node || root;
+			while (current.left !== null) {
 				current = current.left;
 			}
-			return current.element;
+			return current;
 		}
 
-		function findMax() {
-			if (isEmpty()) {
-				throw new Error('removeMin(): Tree is empty.');
-			}
-			var current = root;
-			while(current.right !== null) {
+		function findMaxNode(node) {
+			var current = node || root;
+			while (current.right !== null) {
 				current = current.right;
 			}
-			return current.element;
+			return current;
+		}
+
+		function findMin(node) {
+			if (isEmpty()) {
+				throw new Error('findMin(): Tree is empty.');
+			}
+			return findMinNode(node).element;
+		}
+
+		function findMax(node) {
+			if (isEmpty()) {
+				throw new Error('findMax(): Tree is empty.');
+			}
+			return findMaxNode(node).element;
 		}
 
 		function isEmpty() {
